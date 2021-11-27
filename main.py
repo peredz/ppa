7b43ab19ae6711aa6badf556afb0a550b779cb04
peredz <94736048+peredz@users.noreply.github.com>
2021-11-27 14:08:32 +1000

    a

--- main.py
+++ main.py
@@ -3,13 +3,32 @@ from PyQt5 import uic
 from sys import argv, exit
 from PyQt5.QtGui import QPainter, QColor, QBrush
 from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class CyrcleVashka(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self):
+        self.resize(674, 547)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(260, 190, 171, 161))
+        self.pushButton.setObjectName("pushButton")
+        self.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.pushButton.setText(_translate("MainWindow", "КРУГИ ТУТ!"))
+
+
+class CyrcleVashka(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('crug.ui', self)
         self.a = False
+        self.setupUi()
         self.setup()
 
     def setup(self):
@@ -18,7 +37,6 @@ class CyrcleVashka(QMainWindow):
         self.pushButton.setStyleSheet("""border-radius : 50; 
                               border : 2px solid black""")
         self.pushButton.clicked.connect(self.cyrclllls)
-
 
     def cyrclllls(self):
         self.pushButton.hide()
@@ -33,7 +51,6 @@ class CyrcleVashka(QMainWindow):
             qp.end()
 
     def drawCyrcled(self, qp):
-
         col = QColor(0, 0, 0)
         col.setNamedColor('#yellow')
         qp.setPen(col)
